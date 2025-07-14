2ac0471c6d3e97420a6e46e79133f190dd6d6145
Pallavi-Madhu <pallavimadhu18@gmail.com>
2025-07-14 06:30:31 +0530

    Bottom Navigation Done

--- Navigation/MainTab.js
+++ Navigation/MainTab.js
@@ -1,6 +1,8 @@
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Home from "../src/pages/Home";
+import Patient from "../src/pages/Patient";
+import Donor from "../src/pages/Donor";
 import Settings from "../src/pages/Settings";
 const Tab = createBottomTabNavigator();
 const MainTab = () => {
@@ -10,6 +12,8 @@ const MainTab = () => {
             screenOptions = {({route}) => {
                 let iconName;
                 if (route.name === "Home") iconName = "home-outline";
+                else if (route.name === "Patient") iconName ="account-outline";
+                else if (route.name === "Donor") iconName = "heart-plus-outline";
                 else if (route.name === "Settings") iconName = "cog-outline";
 
                 return {
@@ -17,16 +21,18 @@ const MainTab = () => {
                     <Icon name={iconName} size={size} color={color} />
                     ),
                     headerShown: false,
-                    
                     tabBarStyle: {
-                    height: 55, // <== custom height in pixels
-                    padding: "auto",
+                    height: 55,
+                    padding: "auto"
                     },
+                    tabBarSafeAreaInsets: { bottom: 8 },
                 };
             }}
             initialRouteName = "Home"
         >
             <Tab.Screen name = "Home" component={Home} />
+            <Tab.Screen name = "Patient" component ={Patient} />
+            <Tab.Screen name = "Donor" component={Donor} />
             <Tab.Screen name = "Settings" component ={Settings} />
         </Tab.Navigator>
         </>
---
+++ src/pages/Patient.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Patient = () => {
+  return (
+    <View>
+      <Text>Patient Screen</Text>
+    </View>
+  );
+};
+
+export default Patient;
