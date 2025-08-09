d563c8bae31038f8104c593057443f8a70efb01d
Niranjana Ajay <ajayniranjana05@gmail.com>
2025-08-09 13:42:21 +0530

    add authentication

--- Navigation/RootNavigator.js
+++ Navigation/RootNavigator.js
@@ -3,8 +3,6 @@ import MainTab from "./MainTab.js";
 import PatientProfiles from "../src/pages/PatientProfiles.jsx"
 import DonorProfile from "../src/pages/DonorProfiles.jsx";
 import Landing from "../src/pages/Landing.jsx";
-import SignIn from "../src/pages/SignIn.jsx";
-import SignUp from "../src/pages/SignUp.jsx";
 import PatientForm from "../src/pages/PatientForm.jsx";
 import DonorForm from "../src/pages/DonorForm.jsx";
 
@@ -13,8 +11,6 @@ const RootNavigator = () => {
     return(
         <Stack.Navigator screenOptions={{ headerShown : false}}>
             <Stack.Screen name = "Landing" component = {Landing}/>
-            {/* <Stack.Screen name = "SignIn" component = {SignIn}/>
-            <Stack.Screen name="SignUp" component={SignUp} /> */}
             <Stack.Screen name = "MainTabs" component = {MainTab}/>
             <Stack.Screen name = "PatientProfiles" component = {PatientProfiles}/>
             <Stack.Screen name = "DonorProfile" component = {DonorProfile}/>
