7da9852f88916d46e6ca42f2304cab44f5a4d07e
Niranjana Ajay <ajayniranjana05@gmail.com>
2025-09-13 11:06:34 +0530

    connect patient details to supabase

---
+++ Store/PatientThunk.js
@@ -0,0 +1,20 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const submitPatient=createAsyncThunk(
+    "patient/submitPatient",
+    async(patientData, {rejectWithValue}) =>{
+        try{
+            const response= await axios.post(
+                "http://192.168.1.84:3000/api/patient",
+                patientData
+            );
+            return response.data;
+        }
+        catch(error){
+            return rejectWithValue(
+                error.response?.data?.error || "Failed to register patient"
+            );
+        }
+    }
+);
